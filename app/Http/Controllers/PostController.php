e2fb9726b2c35e735c839bc50c992fbb1a1c093e
raofulcse-art <raoful.cse@gmail.com>
2026-03-17 12:37:05 +0600

    Second Commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
+
 
 use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
+
+use Illuminate\Http\Resource;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
@@ -12,16 +18,25 @@ class PostController extends Controller
     public function index()
     {
         //
-        
+        return PostResource::collection(Post::all());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
         //
-        return $request->only('title');
+        $inputvalues = $request->validated();
+        $inputvalues['title'] = strip_tags($inputvalues['title']);
+        $inputvalues['body'] = strip_tags($inputvalues['body']);
+        $inputvalues['user_id'] = 1;
+
+        $post = Post::create($inputvalues);
+
+        return response()->json($post,201);
+
+        //return $request->only('title');
         //return $request->all();
         /*
         return response()->json([
@@ -35,31 +50,45 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
         //
+        /*
         return response()->json([
             'id' => 1,
             'name' => 'Dipto',
             'age' => 20
         ])
         ->header('Test','Zura');
-        ;
+        ;*/
+        //$post = Post::findorFail($id);
+
+        return response()->json($post);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Post $post)
     {
         //
+        $inputvalues = $request->validate([
+            'title' => ['required' , 'min:5'],
+            'body' => ['required']
+        ]);
+
+        $post->update($inputvalues);
+
+        return response()->json($post);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
         //
+        $post->delete();
+        return response()->noContent();
     }
 }
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => $this->created_at->format('Y-m-d H:1:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:1:s')
+        ];
+    }
+}
