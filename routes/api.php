be08ac0ea833075b6e3898d9ce3cc8216d131a4b
raofulcse-art <raoful.cse@gmail.com>
2026-03-21 01:30:42 +0600

    Breeze Installed

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
 Route::get('/hello' , function(){
     return [ 'message' => 'Hello World'];
     //return 'Hello Wrold';
@@ -12,3 +15,4 @@ Route::get('/hello' , function(){
 //Route::get('/show/{id}',[PostController::class,'show'])->name('posts.show');
 
 Route::apiResource('posts',PostController::class);
+require __DIR__.'/auth.php';
